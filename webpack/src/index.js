78bd409618a2af67212582390d7462c9f8ec2f11
Bryant Meskill <bryant.meskill@gmail.com>
2022-02-13 08:00:26 -0600

    Webpack practice

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,10 +1,31 @@
-import _ from "lodash";
+//import your function
+import myName from "./myName";
+import "./style.css";
+import Icon from "./icon.png";
+import Data from "./data.xml";
+import Notes from "./data.csv";
+import printMe from "./print.js";
 
 function component() {
   const element = document.createElement("div");
+  const btn = document.createElement("button");
 
-  // Lodash, imported from this script
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
+  //use your function!
+  element.textContent = myName("Bryant");
+  element.classList.add("hello");
+
+  btn.innerHTML = "Click me, and check the console!";
+  btn.onclick = printMe;
+
+  //add the image to our current div
+  const myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
+  element.appendChild(btn);
+
+  console.log(Data);
+  console.log(Notes);
 
   return element;
 }
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,9 +1,60 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  mode: "development",
   entry: "./src/index.js",
+  entry: {
+    index: "./src/index.js",
+    print: "./src/print.js",
+  },
+  //allows you to track down bug to source file
+  devtool: "inline-source-map",
+  //tells 'webpack-dev-server' to serve files from the dist directory on localhost:8080
+
+  devServer: {
+    static: "./dist",
+  },
+  //automatically adds bundles to a newly generated HTML file
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Development",
+    }),
+  ],
   output: {
-    filename: "main.js",
+    filename: "bundle.js",
+    //dynamically generating bundle names
+    filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
+    //removes files not in use automatically
+    clean: true,
+  },
+  module: {
+    rules: [
+      //loading css files
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      //loading image files
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      //loading font files
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+      //loading misc data files
+      {
+        test: /\.(csv|tsv)$/i,
+        use: ["csv-loader"],
+      },
+      {
+        test: /\.xml$/i,
+        use: ["xml-loader"],
+      },
+    ],
   },
 };
