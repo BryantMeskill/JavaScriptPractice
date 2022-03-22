1631abbd3447cab35f16834a6ff05d5d1bc65613
Bryant Meskill <bryant.meskill@gmail.com>
2022-03-22 06:00:17 -0500

    Added helper function to reduce code.

--- menu/script.js
+++ menu/script.js
@@ -1,10 +1,21 @@
 const browse = document.querySelector(".browse");
 const browseBtns = document.querySelectorAll(".browse-btn");
+const favorites = document.querySelector(".favorites");
+const favoriteBtns = document.querySelectorAll(".favorite-btn");
+const settings = document.querySelector(".settings");
+const settingsBtns = document.querySelectorAll(".setting-btn");
 
-browse.addEventListener("mouseover", () => {
-  browseBtns.forEach((element) => element.classList.remove("hidden"));
-});
+//TODO: ANIMATE BUTTONS SLIDING DOWN ON HOVER
+function menuHover(container, buttonArray) {
+  container.addEventListener("mouseover", () => {
+    buttonArray.forEach((element) => element.classList.remove("hidden"));
+  });
 
-browse.addEventListener("mouseleave", () => {
-  browseBtns.forEach((element) => element.classList.add("hidden"));
-});
+  container.addEventListener("mouseleave", () => {
+    buttonArray.forEach((element) => element.classList.add("hidden"));
+  });
+}
+
+menuHover(browse, browseBtns);
+menuHover(favorites, favoriteBtns);
+menuHover(settings, settingsBtns);
