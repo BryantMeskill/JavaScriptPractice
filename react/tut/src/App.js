a6532389a969de38d64b3bcd0522e8b369c6aedb
Bryant Meskill <bryant.meskill@gmail.com>
2022-03-26 08:10:25 -0500

    Basic testing.

--- react/tut/src/App.js
+++ react/tut/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ReactDOM } from "react";
+
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Welcome name="Dane" />
+      <Welcome name="Bartholomew" />
+      <Welcome name="Bryant" />
     </div>
   );
 }
