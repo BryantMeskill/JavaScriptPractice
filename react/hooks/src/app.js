61abc0572ce7579161bfb525e3b807d9c8a42738
Bryant Meskill <bryant.meskill@gmail.com>
2022-04-12 09:43:24 -0500

    Additional practice from React docs.

--- react/hooks/src/app.js
+++ react/hooks/src/app.js
@@ -1,40 +1,16 @@
 import React, { useState, useEffect } from "react";
 
 const App = () => {
-  const [color, setColor] = useState("black");
+  const [count, setCount] = useState(0);
 
   useEffect(() => {
-    const changeColorOnClick = () => {
-      if (color === "black") {
-        setColor("red");
-      } else {
-        setColor("black");
-      }
-    };
-
-    document.addEventListener("click", changeColorOnClick);
-
-    return () => {
-      document.removeEventListener("click", changeColorOnClick);
-    };
-  }, [color]);
+    document.title = `You clicked ${count} times`;
+  });
 
   return (
     <div>
-      <div
-        id="myDiv"
-        style={{
-          color: "white",
-          width: "100px",
-          height: "100px",
-          position: "absolute",
-          left: "50%",
-          top: "50%",
-          backgroundColor: color,
-        }}
-      >
-        This div can change color, click on me!
-      </div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
     </div>
   );
 };
