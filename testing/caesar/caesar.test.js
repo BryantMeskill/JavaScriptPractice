df5a3d85b97fb44a2593612c130b6b0eda37ff1a
Bryant Meskill <bryant.meskill@gmail.com>
2022-05-06 15:50:54 -0500

    Removed caesar test, updated launch.json.

--- testing/caesar/caesar.test.js
+++
@@ -1,5 +0,0 @@
-import caesar from "./caesar";
-
-test("Prints the cipher of 'hello' a key of 2 added to it, making it jgnnq", () => {
-  expect(caesar("hello", 2)).toBe("jgnnq");
-});
--- testing/caesar/isUpper.js
+++
@@ -1,5 +0,0 @@
-const isUpper = (string, key) => {
-  return ((string[i] - "A" + key) % 26) + "A";
-};
-
-export default isUpper;
